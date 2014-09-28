82579ea8353450c26d67f040bb6617e52bf03724
Abraham Xiao <q289196573@gmail.com>
2014-09-29 02:25:27 +0400

    Update notes and homework. When you are in doubt, either consult people or Internet. For Internet, I mean a sort of media that can help you walk through the problem. For this homework, ask your classmates for help.

---
+++ Labs/auto/Homework1.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "Homework1"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "url"
+     "graphicx"
+     "array"
+     "algorithmic"
+     "cite"
+     "amssymb"
+     "amsmath"
+     ""
+     "latex2e"
+     "art12"
+     "article"
+     "12pt")))
+
---
+++ Lecture-Notes/auto/Sep.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook "Sep"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "amssymb"
+     "wasysym"
+     "marvosym"
+     "textcomp"
+     "amsmath"
+     "ulem"
+     "normalem"
+     "rotating"
+     "wrapfig"
+     "float"
+     "longtable"
+     "graphicx"
+     "fixltx2e"
+     ""
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "presentation")))
+
