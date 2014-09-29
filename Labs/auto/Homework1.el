93a1a13d3a1119babdef2d19773afa7a3d4508ab
Abraham Xiao <q289196573@gmail.com>
2014-09-29 20:46:32 +0400

    Update homework and course notes. It is shameful to surpass the due date and it is double shameful to get kicked by Masdar Institute. If you do not want so much sunk cost, it is of my best to try whatsoever I can, to prove to those smart and not-so-smart people, that I am a GENIUS.

--- Labs/auto/Homework1.el
+++ Labs/auto/Homework1.el
@@ -1,15 +1,26 @@
 (TeX-add-style-hook "Homework1"
  (lambda ()
+    (LaTeX-add-environments
+     "Law")
     (LaTeX-add-labels
      "sec:question-1"
-     "sec:question-2")
+     "sec:question-2"
+     "eq:1"
+     "eq:2"
+     "eq:3"
+     "sec:question-3"
+     "eq:4"
+     "sec:question-4"
+     "sec:question-5")
     (TeX-run-style-hooks
      "hyperref"
      "url"
      "graphicx"
      "array"
+     "ntheorem"
      "algorithmic"
      "cite"
+     "gensymb"
      "amssymb"
      "amsmath"
      ""
